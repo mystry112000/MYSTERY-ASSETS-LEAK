df2178109d6971678e2909b9bb30f5eb44e9af14
mystry112000 <mystry112000@gmail.com>
2026-06-03 00:13:44 +0530

    Rename to .luau to match playvoras extension

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/mystry112000/MYSTERY-ASSETS-LEAK/main/saveinstance.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mystry112000/MYSTERY-ASSETS-LEAK/main/saveinstance.luau", true))()
